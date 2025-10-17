8c61c852ddf2bfe40f03074abfc0e58946b51273
estudiante <estudiante@oretania-PC>
2025-10-17 09:09:13 +0200

    version 1.1

---
+++ Bienvenida/src/es/iesoretania/saludo/Adios.java
@@ -0,0 +1,11 @@
+package es.iesoretania.saludo;
+
+public class Adios {
+	public static void main(String[] args) {
+		
+		System.out.println("Adios mundo");
+	}
+
+}
+
+}
--- Bienvenida/src/es/iesoretania/saludo/Hola.java
+++ Bienvenida/src/es/iesoretania/saludo/Hola.java
@@ -5,6 +5,8 @@ public class Hola {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
 	}
 
 }
